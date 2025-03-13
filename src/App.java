eb0ec6a219c4b3a3051bb764e9a0c347305627f6
niicolasaps <nicolasalmeida.ps@gmail.com>
2025-03-13 02:51:14 -0300

    codigo

--- src/App.java
+++ src/App.java
@@ -1,41 +1,12 @@
 import java.util.Random;
 
-/** 
- * MIT License
- *
- * Copyright(c) 2024-255 João Caram <caram@pucminas.br>
- *                       Eveline Alonso Veloso
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 public class App {
     static int[] tamanhosTesteGrande =  { 125_000, 250_000, 500_000, 1_000_000, 2_000_000 };
     static int[] tamanhosTesteMedio =   {  12_500,  25_000,  50_000,   100_000,   200_000 };
-    static int[] tamanhosTestePequeno = {       3,       6,      12,        24,        48 };
+    static int[] tamanhosTestePequeno = {       3,       6,      12,        24,        36 };
     static Random aleatorio = new Random(42);
 
-    /**
-     * Código de teste 1. Este método...
-     * @param vetor Vetor com dados para teste.
-     * @return Uma resposta que significa....
-     */
+    
     static int codigo1(int[] vetor) {
         int resposta = 0;
         for (int i = 0; i < vetor.length; i += 2) {
@@ -44,11 +15,7 @@ public class App {
         return resposta;
     }
 
-    /**
-     * Código de teste 2. Este método...
-     * @param vetor Vetor com dados para teste.
-     * @return Uma resposta que significa....
-     */
+    
     static int codigo2(int[] vetor) {
         int contador = 0;
         for (int k = (vetor.length - 1); k > 0; k /= 2) {
@@ -60,10 +27,7 @@ public class App {
         return contador;
     }
 
-    /**
-     * Código de teste 3. Este método...
-     * @param vetor Vetor com dados para teste.
-     */
+    
     static void codigo3(int[] vetor) {
         for (int i = 0; i < vetor.length - 1; i++) {
             int menor = i;
@@ -77,11 +41,7 @@ public class App {
         }
     }
 
-    /**
-     * Código de teste 4 (recursivo). Este método...
-     * @param n Ponto inicial do algoritmo
-     * @return Um inteiro que significa...
-     */
+    
     static int codigo4(int n) {
         if (n <= 2)
             return 1;
@@ -89,11 +49,7 @@ public class App {
             return codigo4(n - 1) + codigo4(n - 2);
     }
 
-    /**
-     * Gerador de vetores aleatórios de tamanho pré-definido. 
-     * @param tamanho Tamanho do vetor a ser criado.
-     * @return Vetor com dados aleatórios, com valores entre 1 e (tamanho/2), desordenado.
-     */
+    
     static int[] gerarVetor(int tamanho){
         int[] vetor = new int[tamanho];
         for (int i = 0; i < tamanho; i++) {
@@ -103,6 +59,54 @@ public class App {
         
     }
     public static void main(String[] args) {
+        long inicio, fim;
+        int operacoes;
+    
         
+        for (int tamanho : tamanhosTesteGrande) {
+            int[] vetor = gerarVetor(tamanho);
+    
+            inicio = System.nanoTime();
+            operacoes = codigo1(vetor);
+            fim = System.nanoTime();
+            System.out.println("codigo1 - " + tamanho + " - " + operacoes + " - " + (fim - inicio));
+    
+            inicio = System.nanoTime();
+            operacoes = codigo2(vetor);
+            fim = System.nanoTime();
+            System.out.println("codigo2 - " + tamanho + " - " + operacoes + " - " + (fim - inicio));
+        }
+    
+        
+        for (int tamanho : tamanhosTesteMedio) {
+            int[] vetor = gerarVetor(tamanho);
+    
+            operacoes = 0;
+            inicio = System.nanoTime();
+            for (int i = 0; i < vetor.length - 1; i++) {
+                int menor = i;
+                for (int j = i + 1; j < vetor.length; j++) {
+                    operacoes++; 
+                    if (vetor[j] < vetor[menor])
+                        menor = j;
+                }
+                
+                int temp = vetor[i];
+                vetor[i] = vetor[menor];
+                vetor[menor] = temp;
+                operacoes += 3; 
+            }
+            fim = System.nanoTime();
+            System.out.println("codigo3 - " + tamanho + " - " + operacoes + " - " + (fim - inicio));
+        }
+    
+        
+        for (int tamanho : tamanhosTestePequeno) {
+            inicio = System.nanoTime();
+            operacoes = codigo4(tamanho);
+            fim = System.nanoTime();
+            System.out.println("codigo4 - " + tamanho + " - " + operacoes + " - " + (fim - inicio));
+        }
     }
+    
 }
